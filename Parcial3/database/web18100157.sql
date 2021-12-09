78aa23e12f44badf877267840a2a8960bdd778a2
Cesar-CCS <CesarCastro_S@outlook.com>
2021-12-09 02:21:30 -0600

    Actualizacion de DB

--- Parcial3/database/web18100157.sql
+++ Parcial3/database/web18100157.sql
@@ -1,32 +1,34 @@
-create database web18100157
+create database web18100157;
 
 use web18100157;
 
 create table Usuario(
+id_Usuario int identity (1, 1) not null ,
 Nombre_Director varchar(20), 
 contraseña varchar (6), 
-primary key(Nombre_Director));
+primary key(id_Usuario));
 
 create table Curso (
-Nombre_Usuario varchar(20),
+id_Curso int identity (1, 1) not null,
 Nombre_Docente varchar(10),
-Apellido varchar (20),
+Apellido varchar (40),
 Nombre_Curso varchar (20),
-Costo_Curso int (4),
+Costo int,
 Correo varchar (50),
 Categoria varchar (20),
 Requisitos varchar (100),
 Descripcion varchar (250),
-foreign key(Nombre_Usuario) references Usuario (Nombre_Director));
+primary key(id_Curso));
 
 insert into Usuario(Nombre_Director, Contraseña) values
-("Rogelio","Rog150"),("Martha","M4rt4");
+('Rogelio','Rog150'),('Martha','M4rt4');
+
 
 insert into Curso 
-(Nombre_Usuario, Nombre_Docente, Apellido, Nombre_Curso, Costo_Curso, Correo, Categoria, Requisitos, Descripcion) values
-("Rogelio","Juan","Rodriguez","HTML Junior",250,"JuanRodri@gmail.com","Programacion Web","Tener un manejador de texto", "En este curso aprenderás lo basico del lenguaje de etiquetas de hipertexto (HTML) para entrar al mundo de la programación web"),
-("Martha","Oracio","Perez","Guitarra 1",300,"Oracles@hotmail.com","Musica","Tener una Guitarra","Hola, soy Oracio y en mi curso de guitarra te enseñare lo basico para que logres enamorarte de este instrumento"),
-("Rogelio","Maria","Gutierrez","Aprendiendo C#",500,"Maria_Correo@outlook.com","Programacion","Conocimientos Basicos en programacion y el IDE Visual Studio", "Hola en este curso te enseñare todo lo que necesitas para convertirte en experto en el lenguaje C#");
+(Nombre_Docente, Apellido, Nombre_Curso, Costo, Correo, Categoria, Requisitos, Descripcion) values
+('Juan','Rodriguez','HTML Junior',250,'JuanRodri@gmail.com','Programacion Web','Tener un manejador de texto', 'En este curso aprenderás lo basico del lenguaje de etiquetas de hipertexto (HTML) para entrar al mundo de la programación web'),
+('Oracio','Perez','Guitarra 1',300,'Oracles@hotmail.com','Musica','Tener una Guitarra','Hola, soy Oracio y en mi curso de guitarra te enseñare lo basico para que logres enamorarte de este instrumento'),
+('Maria','Gutierrez','Aprendiendo C#',500,'Maria_Correo@outlook.com','Programacion','Conocimientos Basicos en programacion y el IDE Visual Studio', 'Hola en este curso te enseñare todo lo que necesitas para convertirte en experto en el lenguaje C#');
 
 
 
